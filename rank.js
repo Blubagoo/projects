d3f5478b1aeb0988a02470b2ac2f2bc6265ec1b4
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2018-04-20 10:02:31 -0500

    initial commit

---
+++ rank.js
@@ -0,0 +1,24 @@
+  const rankResults = [
+  	{
+  		ranking: 'I guess you\'re no better than a gOoMbA...',
+  		img: 'https://www.ssbwiki.com/images/5/5d/Goomba-Brawl.png',
+  		alt: 'A goomba from the famous Super Mario Brother Series'
+  	},
+  	{
+  		ranking: 'At least you made it to the ranking of some unknown raider destined to be killed by the main protagonist.',
+  		img: 'http://www.gameinformer.com/resized-image.ashx/__size/500x0/__key/CommunityServer-Components-UserFiles/00-00-36-50-71-Attached+Files/4454.56_2700_.jpg',
+  		alt: 'A nameless raider from the game Borderlands'
+  	},
+  	{
+  		ranking: 'Sweet! You made it to the ranking of the forever famous Pilgor! The destroyer of humans and their things.',
+  		img: 'https://vignette.wikia.nocookie.net/vsbattles/images/3/33/Pilgor_Goat_Simulator.png/revision/latest?cb=20160617200018',
+  		alt: 'Pilgor from the game Goat Simulator'
+  	},
+  	{
+  		ranking: 'You are a god among men... Welcome Kratos the God of War!',
+  		img: 'http://media.comicbook.com/2018/01/god-of-war-1073040.jpeg',
+  		alt: 'Kratos roaring after a victory'
+  	}
+
+
+  ]
